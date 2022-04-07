acffe51ed623253ceb0a05920e6758bc46990a32
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-07 06:06:34 +0000

    Chaged App.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route("/") 
 def hello_world(): 
-	return "<p>Hello, World!</p>"
+	return "<p>Hello, World from the Container!</p>"
 
 
 if __name__ == '__main__':
